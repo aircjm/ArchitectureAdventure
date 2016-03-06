ad7efe9d08cf3583678a629c7e0c13f2fbe4cbb8
aircjm <aircjm@qq.com>
2016-03-06 18:08:25 +0800

    创建实体类model层建立

---
+++ src/test/java/com/aircjm/web/CustomerServiceTest.java
@@ -0,0 +1,80 @@
+package com.aircjm.web;
+
+import com.aircjm.web.model.Customer;
+import com.aircjm.web.service.CustomerService;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * <Description> <br>
+ *
+ * @author chenjiaming<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate 2016/3/6 <br>
+ */
+public class CustomerServiceTest {
+
+    private final CustomerService customerService;
+
+    public CustomerServiceTest() {
+        customerService = new CustomerService();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCustomerList() throws Exception {
+        List<Customer> customerList = customerService.getCustomerList("");
+        Assert.assertEquals(2, customerList.size());
+    }
+
+    @Test
+    public void testGetCustomer() throws Exception {
+        long id = 1;
+        Customer customer = customerService.getCustomer(id);
+        Assert.assertNotNull(customer);
+    }
+
+    @Test
+    public void testCreateCustomer() throws Exception {
+        Map<String, Object> fieldMap = new HashMap<String, Object>();
+        fieldMap.put("name", "customer100");
+        fieldMap.put("contact", "John");
+        fieldMap.put("telephone", "15425876542");
+        boolean result = customerService.createCustomer(fieldMap);
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void testUpdateCustomer() throws Exception {
+        long id = 1;
+        Map<String, Object> fieldMap = new HashMap<String, Object>();
+        fieldMap.put("contact", "Eric");
+        boolean result = customerService.updateCustomer(id,fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testDeleteCustomer() throws Exception {
+        long id = 1;
+        boolean result = customerService.deleteCustomer(id);
+        Assert.assertTrue(result);
+    }
+}
