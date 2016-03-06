0f9d059d7c3cce4ffe8a807fe20290af84f337ac
aircjm <aircjm@qq.com>
2016-03-06 18:09:32 +0800

    创建Service文件,Service层创建

---
+++ src/main/java/com/aircjm/web/service/CustomerService.java
@@ -0,0 +1,41 @@
+/****************************************************************************************
+ *
+ ****************************************************************************************/
+package com.aircjm.web.service;
+
+import com.aircjm.web.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <提供客户数据服务> <br>
+ *
+ * @author chenjiaming<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate 2016/3/6 <br>
+ */
+public class CustomerService {
+
+    public List<Customer> getCustomerList(String keyword) {
+        return null;
+    }
+
+    public Customer getCustomer(long id) {
+        return null;
+    }
+
+    public boolean createCustomer(Map<String, Object> fieldMap) {
+        return false;
+    }
+
+    public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
+        return false;
+    }
+
+    public boolean deleteCustomer(long id) {
+        return false;
+    }
+
+}
