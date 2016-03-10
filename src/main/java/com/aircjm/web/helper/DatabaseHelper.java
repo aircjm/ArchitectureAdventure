1519ec50833135f3a486667395226c624e2af42a
aircjm <aircjm@qq.com>
2016-03-10 16:29:23 +0800

    添加更多的数据库操作工具方法

--- src/main/java/com/aircjm/web/helper/DatabaseHelper.java
+++ src/main/java/com/aircjm/web/helper/DatabaseHelper.java
@@ -1,16 +1,21 @@
 
 package com.aircjm.web.helper;
 
+import com.aircjm.web.util.CollectionUtil;
 import com.aircjm.web.util.PropsUtil;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -61,13 +66,13 @@ public final class DatabaseHelper {
      */
     public static Connection getConnection() {
         Connection connection = CONNECTION_THREAD_LOCAL.get();
-        if (connection == null){
+        if (connection == null) {
             try {
                 connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             } catch (SQLException e) {
                 LOGGER.error("get Connection failure", e);
                 throw new RuntimeException(e);
-           } finally {
+            } finally {
                 CONNECTION_THREAD_LOCAL.set(connection);
             }
         }
@@ -76,6 +81,7 @@ public final class DatabaseHelper {
 
     /**
      * 关闭数据库连接
+     * 
      * @param connection
      */
     public static void closeConnection(Connection connection) {
@@ -88,7 +94,7 @@ public final class DatabaseHelper {
         }
     }
 
-    public static void closeConnection(){
+    public static void closeConnection() {
         Connection conn = CONNECTION_THREAD_LOCAL.get();
         if (conn != null) {
             try {
@@ -96,8 +102,8 @@ public final class DatabaseHelper {
             } catch (SQLException e) {
                 LOGGER.error("close Connection error", e);
                 throw new RuntimeException(e);
-            }finally {
-                //使用完之后移除Connection
+            } finally {
+                // 使用完之后移除Connection
                 CONNECTION_THREAD_LOCAL.remove();
             }
         }
@@ -105,18 +111,19 @@ public final class DatabaseHelper {
 
     /**
      * 查询实体列表
+     * 
      * @param entityClass
      * @param sql
      * @param params
      * @param <T>
      * @return
      */
-    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params){
+    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params) {
         List<T> entityList;
         Connection connection = getConnection();
-        try{
+        try {
             entityList = QUERY_RUNNER.query(connection, sql, new BeanListHandler<T>(entityClass), params);
-        } catch (SQLException e){
+        } catch (SQLException e) {
             LOGGER.error("query list failure", e);
             throw new RuntimeException(e);
         } finally {
@@ -125,4 +132,151 @@ public final class DatabaseHelper {
         return entityList;
     }
 
+    /**
+     * 查询实体
+     * 
+     * @param entityClass
+     * @param sql
+     * @param params
+     * @param <T>
+     * @return
+     */
+    public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params) {
+        T entity;
+        try {
+            Connection conn = getConnection();
+            entity = QUERY_RUNNER.query(conn, sql, new BeanHandler<T>(entityClass), params);
+        } catch (SQLException e) {
+            LOGGER.error("query entity Failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return entity;
+    }
+
+    /**
+     * 执行查询语句
+     * 
+     * @param sql 查询sql
+     * @param params 传入参数
+     * @return List<Map<String, Object>> 对象的键值对
+     */
+    public static List<Map<String, Object>> executeQuery(String sql, Object... params) {
+
+        List<Map<String, Object>> result;
+        Connection connection = getConnection();
+
+        try {
+            result = QUERY_RUNNER.query(connection, sql, new MapListHandler(), params);
+        } catch (SQLException e) {
+            LOGGER.error("execute query failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+
+        return result;
+    }
+
+    /**
+     * 执行SQL
+     * 
+     * @param sql Sql语句
+     * @param params 参数
+     * @return 是否成功 修改了几行记录
+     */
+    public static int executeUpdate(String sql, Object... params) {
+        int rows = 0;
+        Connection connection = getConnection();
+        try {
+            rows = QUERY_RUNNER.update(connection, sql, params);
+        } catch (SQLException e) {
+            LOGGER.error("update SQL error", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return rows;
+    }
+
+    /**
+     * 插入实体
+     * 
+     * @param entityClass
+     * @param fieldMap
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean insertEntity(Class<T> entityClass, Map<String, Object> fieldMap) {
+        /* 判断有无实体插入 */
+        if (CollectionUtil.isEmpty(fieldMap)) {
+            LOGGER.error("fieldMap is empty, can not insert entity");
+            return false;
+        }
+
+        String sql = "insert into " + getTableName(entityClass);
+        StringBuilder columns = new StringBuilder(" (");
+        StringBuilder values = new StringBuilder(" (");
+        for (String fieldName : fieldMap.keySet()) {
+            columns.append(fieldName).append(", ");
+            values.append("?, ");
+        }
+
+        columns.replace(columns.lastIndexOf(", "), columns.length(), ")");
+        values.replace(values.lastIndexOf(", "), values.length(), ")");
+        sql += columns + " values " + values;
+
+        Object[] params = fieldMap.values().toArray();
+        return executeUpdate(sql, params) == 1;
+
+    }
+
+    public static <T> boolean updateEntity(Class<T> entityClass, long id, Map<String, Object> fieldMap) {
+        /* 判断有无实体插入 */
+        if (CollectionUtil.isEmpty(fieldMap)) {
+            LOGGER.error("fieldMap is empty, can not update entity");
+            return false;
+        }
+
+        String sql = "update " + getTableName(entityClass) + " SET ";
+        StringBuilder columns = new StringBuilder();
+
+        for (String fieldName : fieldMap.keySet()) {
+            columns.append(fieldName).append("=?, ");
+        }
+
+        sql += columns.substring(0, columns.lastIndexOf(", ")) + " where id=?";
+
+        List<Object> paramList = new ArrayList<Object>();
+        paramList.addAll(fieldMap.values());
+        paramList.add(id);
+
+        Object[] params = paramList.toArray();
+        return executeUpdate(sql, params) == 1;
+
+    }
+
+    /**
+     * 删除实体对象
+     * 
+     * @param entityClass 实体对象
+     * @param id id
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean deleteEntity(Class<T> entityClass, long id) {
+        String sql = "delete from " + getTableName(entityClass) + " where id = ?";
+        return executeUpdate(sql, id) == 1;
+    }
+
+    /**
+     * 根据实体类名获取表名
+     * 
+     * @param entityClass
+     * @return
+     */
+    private static String getTableName(Class<?> entityClass) {
+        return entityClass.getSimpleName().toLowerCase();
+    }
 }
